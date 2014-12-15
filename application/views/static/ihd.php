b3ce894621a683e92b113f2e50a9ec4cb6d1993e
Mindy <mindyh@stanford.edu>
2014-12-14 19:04:23 -0800

    deleted old items, updated db, added calendar and events, pruned dead links and added new ones, and basically cleaned up a whole bunch of functionality

---
+++ application/views/static/ihd.php
@@ -0,0 +1,57 @@
+<div class="panel">
+	<h4>What is In-House Draw?</h4>
+	<p>It's an event where next year's sophomores choose their rooms.</p>
+
+	<h4>Who should go to In-House Draw?</h4>
+	<p>All sophomores preassigned to FroSoCo for 2014-2015 should attend In-House Draw or send a proxy to choose a room for next year. 
+		If you are not represented, you will be assigned the leftover room.</p>
+
+	<h4>What forms do I need?</h4>
+	<p>Fill out the <a href="/assets/documents/InHouse_Draw_Form_2014-15.pdf">2014-2015 In-House Draw form</a>. 
+	If you have a proxy representing you, make sure to fill out the <a href="/assets/documents/proxyform.pdf">proxy form</a> as well,
+	 and give your proxy both forms to turn in.</p>
+
+	<h4>When and where is In-House Draw?</h4>
+	<p><b>June 2nd, 9pm, in Ricker.</b> Don't be late, or we might skip over you!</p>
+
+	<h4>Will there be food?</h4>
+	<p>Yes. Be there.</p>
+
+	<h4>What rooms are available next year?</h4>
+	<p>The list of rooms is available for <a href="/assets/documents/RoomList.pdf">download here</a>.</p>
+
+	<h4>What are the rules for In-House Draw?</h4>
+	<p><a href="/assets/documents/2014-15_In-House_Draw_Overview.pdf">A document of official rules is linked here.</a> 
+	Note that the section about Gender Neutral housing does not apply to FroSoCo.</p>
+
+	<h4>I want a single, what should I do?</h4>
+	<p>Fill in <a href="https://docs.google.com/a/stanford.edu/forms/d/1NDRgEhw8b82jNgs50fRbT2pRSp_fvVVWrGIZdPXoWnY/viewform">
+        this form</a>, and we'll let you know whether your draw number is good enough to be guaranteed a single.</p>
+
+	<h4>I need a roommate! What should I do?</h4>
+	<p>Attend the mixer at <b>8:30pm in Schiff lounge</b> and see if you can't nab one.</p>
+
+	<h4>I have more questions! Who should I contact?</h4>
+	<p>Kay Dannenmaier, your awesome OCT next year, is the point person for all your In-House Draw questions! 
+	Email her at kdanne@stanford.edu.</p>
+
+    <h4>You don't sound like Roger, who are you?</h4>
+    <p>Hi! I'm Mindy, your RMC for next year. The in-house draw is run by next year's staff, and we look forward
+    to meeting you all!</p>
+
+</div>
+
+<div class="panel">
+	<div class="alert alert-info">
+		<p><strong>Downloads</strong></p>
+		<p><a href="/assets/documents/2014-15_In-House_Draw_Overview.pdf">General Rules for In-House Draw</a></p>
+		<p><a href="/assets/documents/InHouse_Draw_Form_2014-15.pdf">In-House Draw Form</a></p>
+		<p><a href="/assets/documents/proxyform.pdf">Proxy Form</a></p>
+        <p><a href="/assets/documents/RoomList.pdf">Rooms for 2014-15</a></p>
+	</div>
+    <div>
+        <p><strong>Links</strong></p>
+        <p><a href="https://docs.google.com/a/stanford.edu/forms/d/1NDRgEhw8b82jNgs50fRbT2pRSp_fvVVWrGIZdPXoWnY/viewform">
+        Singles Application Form</a></p>
+    </div>
+</div>
